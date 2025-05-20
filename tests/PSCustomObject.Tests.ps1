ecd16e207d663d48073deef9caaf67bcb39e79a9
Marius Storhaug <marstor@hotmail.com>
2025-05-21 00:00:58 +0200

    Add tests for Compare-PSCustomObject function with and without -OnlyChanged parameter

--- tests/PSCustomObject.Tests.ps1
+++ tests/PSCustomObject.Tests.ps1
@@ -23,9 +23,30 @@ Describe 'PSCustomObject' {
 
         $diff = $Left | Compare-PSCustomObject $Right
 
+        $diff | Should -HaveCount 2
         $diff.Property | Should -BeIn @('Name', 'Value')
         $diff.Left | Should -BeIn @('Test', 1)
         $diff.Right | Should -BeIn @('Test', 2)
         $diff.Changed | Should -Contain $true
+    }
+
+    It 'Function: Compare-PSCustomObject -OnlyChanged' {
+        $Left = [PSCustomObject]@{
+            Name  = 'Test'
+            Value = 1
+        }
+
+        $Right = [PSCustomObject]@{
+            Name  = 'Test'
+            Value = 2
+        }
+
+        $diff = $Left | Compare-PSCustomObject $Right -OnlyChanged
+
+        $diff | Should -HaveCount 1
+        $diff.Property | Should -Be 'Value'
+        $diff.Left | Should -Be 1
+        $diff.Right | Should -Be 2
+        $diff.Changed | Should -Be $true
     }
 }
