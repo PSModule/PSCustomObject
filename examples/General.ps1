f29b42e3d9bb4aa8d1a368dac937b0cf468dd8b6
Marius Storhaug <marstor@hotmail.com>
2025-05-20 23:29:31 +0200

    Update README and examples for PSCustomObject module; add Compare-PSCustomObject usage and tests

--- examples/General.ps1
+++ examples/General.ps1
@@ -1,1 +1,17 @@
-﻿
+﻿$object1 = [PSCustomObject]@{
+    Name  = 'Test'
+    Value = 1
+}
+
+$object2 = [PSCustomObject]@{
+    Name  = 'Test'
+    Value = 2
+}
+
+$object1 | Compare-PSCustomObject $object2
+
+# Output:
+# Property Left Right Changed
+# -------- ---- ----- -------
+# Name     Test Test    False
+# Value    1    2        True
---
+++ tests/PSCustomObject.Tests.ps1
@@ -0,0 +1,31 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'PSCustomObject' {
+    It 'Function: Compare-PSCustomObject' {
+        $Left = [PSCustomObject]@{
+            Name  = 'Test'
+            Value = 1
+        }
+
+        $Right = [PSCustomObject]@{
+            Name  = 'Test'
+            Value = 2
+        }
+
+        $diff = $Left | Compare-PSCustomObject $Right
+
+        $diff.Property | Should -BeIn @('Name', 'Value')
+        $diff.Left | Should -BeIn @('Test', 1)
+        $diff.Right | Should -BeIn @('Test', 2)
+        $diff.Changed | Should -Contain $true
+    }
+}
