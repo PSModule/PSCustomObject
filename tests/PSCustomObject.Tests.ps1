1959a966194242be6789492ebbf7dfa52497d750
Marius Storhaug <marstor@hotmail.com>
2025-05-21 00:17:19 +0200

    Refactor suppression messages in tests for clarity and consistency

--- tests/PSCustomObject.Tests.ps1
+++ tests/PSCustomObject.Tests.ps1
@@ -1,10 +1,20 @@
-﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
-    'PSReviewUnusedParameter', '',
-    Justification = 'Required for Pester tests'
-)]
+﻿#Requires -Modules @{ ModuleName = 'Pester'; RequiredVersion = '5.7.1' }
+
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
     'PSUseDeclaredVarsMoreThanAssignments', '',
-    Justification = 'Required for Pester tests'
+    Justification = 'Pester grouping syntax: known issue.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingConvertToSecureStringWithPlainText', '',
+    Justification = 'Used to create a secure string for testing.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidUsingWriteHost', '',
+    Justification = 'Log outputs to GitHub Actions logs.'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSAvoidLongLines', '',
+    Justification = 'Long test descriptions and skip switches'
 )]
 [CmdletBinding()]
 param()
